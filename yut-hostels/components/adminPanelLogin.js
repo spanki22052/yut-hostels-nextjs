2d8e3fd774c334aa3c6a5a42ee89aac71ecf24f8
spanki22052 <useful22052@gmail.com>
2020-09-14 22:18:52 +0300

    add admin page and admin panel login component

---
+++ yut-hostels/components/adminPanelLogin.js
@@ -0,0 +1,7 @@
+export default function AdminPanelLogin() {
+    return(
+        <div>
+            <h4 style={{ fontFamily: "Montserrat, sans-serif"}}>Hello from admin panel login component</h4>
+        </div>
+    )
+}
---
+++ yut-hostels/pages/admin.js
@@ -0,0 +1,9 @@
+import AdminPanelLogin from "../components/adminPanelLogin";
+export default function AdminPage() {
+  return (
+    <div>
+      <h1 style={{ fontFamily: "Montserrat, sans-serif"}}>hello from admin page</h1>
+      <AdminPanelLogin />
+    </div>
+  );
+}
