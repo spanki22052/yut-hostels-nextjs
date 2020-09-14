b0e79c05d1ed883d2cc0858c55bc8a9feb4c3749
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-09-14 21:47:17 +0300

    add Title and Suptitle

--- yut-hostels/pages/index.js
+++ yut-hostels/pages/index.js
@@ -2,8 +2,30 @@ import Nav from "../components/nav";
 
 export default function MainPage() {
 	return (
-		<Nav>
+		<>
+			<Nav>
+				<div className='title'>Привет, дорогой гость!</div>
+				<div className='suptitle'>Ищешь комфортный, приятный, хостел, расположенный около метро?</div>
+			</Nav >
+			<style jsx>{`
+				.title {
+					font-family: "Montserrat", sans-serif;
+					font-style: normal;
+					font-weight: 600;
+					font-size: 34px;
+					line-height: 41px;
+					margin-bottom: 5px;
+				}
 
-		</Nav>
+				.suptitle {
+					font-family: "Montserrat", sans-serif;
+					font-style: normal;
+					font-weight: 500;
+					font-size: 26px;
+					line-height: 32px;
+				}
+	`}</style>
+		</>
 	)
 }
+
