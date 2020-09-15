946a967dd2730f3f58a7f62c90b8a5e1cb2149d0
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-09-15 14:16:56 +0300

    change card svg

--- yut-hostels/components/cards.js
+++ yut-hostels/components/cards.js
@@ -1,19 +1,17 @@
 import classes from "../styles/cards.module.scss";
 
 export default function Card() {
-  return (
-    <>
-      <div className={classes.cardBlock}>
-        <div className={classes.cardTitle}>Диско</div>
-        <div className={classes.cardSupTitle}>Количество свободных мест</div>
-        <div className={classes.cardProgressBar}>
-          <div className={classes.Progress}></div>
-        </div>
-        <div>75%</div>
-        <div className={classes.icon}>
-          <img src="/svg/strelka.svg" />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={classes.cardBlock}>
+				<div className={classes.cardTitle}>Диско</div>
+				<div className={classes.cardSupTitle}>Количество свободных мест</div>
+				<div className={classes.cardProgressBar}>
+					<div className={classes.Progress}></div>
+				</div>
+				<div>75%</div>
+				<img className={classes.icon} src="/svg/play.svg" />
+			</div>
+		</>
+	);
 }
