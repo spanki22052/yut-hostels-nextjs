9b366c74db5e4f981b0687e3222a3d7cbe064d61
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-09-15 21:17:33 +0300

    some changes

---
+++ yut-hostels/pages/hostelListPage.js
@@ -0,0 +1,14 @@
+import Nav from "../components/nav";
+import SearchBar from "../components/searchBar";
+import CardsFilter from "../components/cardsFilter";
+
+export default function HostelListPage() {
+	return (
+		<>
+			<Nav>
+				<SearchBar />
+				<CardsFilter />
+			</Nav>
+		</>
+	)
+}
