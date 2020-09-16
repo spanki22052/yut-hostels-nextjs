6e703866d8d8dc1f00eb49a009625ba6864e9471
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-09-16 23:18:01 +0300

    for boss dick

--- yut-hostels/components/nav.js
+++ yut-hostels/components/nav.js
@@ -3,46 +3,46 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Nav({ children }) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  return (
-    <div className={classes.wrapper}>
-      <div className={classes.container}>
-        <nav className={classes.nav}>
-          <Link href="/">
-            <a>
-              <div className={classes.navLogo}>Уют</div>
-            </a>
-          </Link>
-          <div
-            className={
-              !isMenuOpen
-                ? classes.navBurger
-                : classes.navBurger + " " + classes.active
-            }
-            onClick={() =>
-              isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)
-            }
-          >
-            <span></span>
-          </div>
-          <div
-            className={
-              isMenuOpen
-                ? classes.navMenu
-                : classes.active
-            }
-          >
-            <Link href="/hostelslist">
-              <a>список хостелов</a>
-            </Link>
-            <Link href="/">
-              <a>о нас</a>
-            </Link>
-          </div>
-        </nav>
-        <main>{children}</main>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classes.wrapper}>
+			<div className={classes.container}>
+				<nav className={classes.nav}>
+					<Link href="/">
+						<a>
+							<div className={classes.navLogo}>Уют</div>
+						</a>
+					</Link>
+					<div
+						className={
+							!isMenuOpen
+								? classes.navBurger
+								: classes.navBurger + " " + classes.active
+						}
+						onClick={() =>
+							isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)
+						}
+					>
+						<span></span>
+					</div>
+					<div
+						className={
+							isMenuOpen
+								? classes.navMenu
+								: classes.active
+						}
+					>
+						<Link href="/hostelslist">
+							<a>список хостелов</a>
+						</Link>
+						<Link href="/">
+							<a>о нас</a>
+						</Link>
+					</div>
+				</nav>
+				<main>{children}</main>
+			</div>
+		</div>
+	);
 }
