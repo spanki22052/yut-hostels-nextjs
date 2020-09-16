c6988949e9fdae30279ef954f9c8b6f500ba3256
spanki22052 <useful22052@gmail.com>
2020-09-16 21:57:06 +0300

    repaired small mistake

--- yut-hostels/components/nav.js
+++ yut-hostels/components/nav.js
@@ -1,36 +1,38 @@
-import classes from '../styles/nav.module.scss';
-import Link from 'next/link';
-import { useState } from 'react';
+import classes from "../styles/nav.module.scss";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Nav({ children }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-	const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const handleClick = () => {
+    isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true);
+    console.log(isMenuOpen);
+  };
 
-	const handleClick = () => {
-		if(isMenuOpen){
-			setIsMenuOpen(true)
-			console.log(isMenuOpen);
-		} else{
-			setIsMenuOpen(false)
-			console.log(isMenuOpen);
-		}
-	}
-
-	return (
-		<div className={classes.wrapper}>
-			<div className={classes.container}>
-				<nav className={classes.nav}>
-					<Link href='/'><a><div className={classes.navLogo}>Уют</div></a></Link>
-					<div className={classes.navBurger} onClick={e => {e.preventDefault(); handleClick();}}><span></span></div>
-					<div className={classes.navMenu}>
-						<Link href='/hostelslist'><a>список хостелов</a></Link>
-						<Link href='/'><a>о нас</a></Link>
-					</div>
-				</nav>
-				<main>
-					{children}
-				</main>
-			</div>
-		</div>
-	)
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <Link href="/">
+            <a>
+              <div className={classes.navLogo}>Уют</div>
+            </a>
+          </Link>
+          <div className={classes.navBurger} onClick={handleClick}>
+            <span></span>
+          </div>
+          <div className={classes.navMenu}>
+            <Link href="/hostelslist">
+              <a>список хостелов</a>
+            </Link>
+            <Link href="/">
+              <a>о нас</a>
+            </Link>
+          </div>
+        </nav>
+        <main>{children}</main>
+      </div>
+    </div>
+  );
 }
