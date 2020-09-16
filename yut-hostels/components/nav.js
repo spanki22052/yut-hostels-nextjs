c6fbd9b04305d65d1fcd97a1782f0673e6173d1d
spanki22052 <useful22052@gmail.com>
2020-09-17 00:06:07 +0300

    pull for secretar

--- yut-hostels/components/nav.js
+++ yut-hostels/components/nav.js
@@ -1,48 +1,52 @@
 import classes from "../styles/nav.module.scss";
 import Link from "next/link";
 import { useState } from "react";
+import HamburgerMenu from "react-hamburger-menu";
 
 export default function Nav({ children }) {
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-	return (
-		<div className={classes.wrapper}>
-			<div className={classes.container}>
-				<nav className={classes.nav}>
-					<Link href="/">
-						<a>
-							<div className={classes.navLogo}>Уют</div>
-						</a>
-					</Link>
-					<div
-						className={
-							!isMenuOpen
-								? classes.navBurger
-								: classes.navBurger + " " + classes.active
-						}
-						onClick={() =>
-							isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)
-						}
-					>
-						<span></span>
-					</div>
-					<div
-						className={
-							isMenuOpen
-								? classes.navMenu
-								: classes.active
-						}
-					>
-						<Link href="/hostelslist">
-							<a>список хостелов</a>
-						</Link>
-						<Link href="/">
-							<a>о нас</a>
-						</Link>
-					</div>
-				</nav>
-				<main>{children}</main>
-			</div>
-		</div>
-	);
+
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <div className={classes.nav}>
+          <Link href="/">
+            <a>
+              <div className={classes.navLogo}>Уют</div>
+            </a>
+          </Link>
+          <div className={classes.navBurger} style={{ zIndex: "3" }}>
+            <HamburgerMenu
+              isOpen={isMenuOpen}
+              menuClicked={() => setIsMenuOpen(!isMenuOpen)}
+              width={25}
+              height={20}
+              strokeWidth={1}
+              rotate={0}
+              color="black"
+              borderRadius={0}
+              animationDuration={0.5}
+            />
+          </div>
+          <div
+            className={
+              !isMenuOpen
+                ? classes.navMenu + " " + classes.active
+                : classes.navMenu + " " + classes.active
+            }
+          >
+            <Link className={"hamb"} href="/hostelslist">
+              <a>список хостелов</a>
+            </Link>
+            <Link href="/">
+              <a>о нас</a>
+            </Link>
+          </div>
+        </div>
+        <main>{children}</main>
+      </div>
+    </div>
+  );
 }
