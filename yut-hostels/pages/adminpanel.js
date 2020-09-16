bd3a433742893ab78c6218166bba0c5423dd3484
spanki22052 <useful22052@gmail.com>
2020-09-16 15:17:24 +0300

    finished parameters input logic

--- yut-hostels/pages/adminpanel.js
+++ yut-hostels/pages/adminpanel.js
@@ -7,6 +7,30 @@ export default function AdminPage() {
   const [currentAdd, setAdd] = useState("");
   const [hostelsList, setHostels] = useState([]);
   const [hostelTitle, setHostelTitle] = useState("");
+  const [hostelParametersObject, setHostelObject] = useState({
+    title: "",
+    description: "",
+    peoplePercentage: 0,
+    phoneNumber: "",
+    metro: "",
+    photos: "",
+  });
+
+  const inputStringsHolder = [
+    "Название хостела",
+    "title",
+    "Описание",
+    "description",
+    "Процент населенности",
+    "peoplePercentage",
+    "Телефон",
+    "phoneNumber",
+    "Метро + HEX",
+    "metro",
+    "Название фото (через пробел)",
+    "photos",
+  ];
+
   const Router = useRouter();
 
   firebase.auth().onAuthStateChanged(function (user) {
@@ -72,7 +96,32 @@ export default function AdminPage() {
           </div>
         ) : (
           <div className={css.hostelBlock}>
-            <h1>Parameters</h1>
+            {inputStringsHolder
+              .filter((el, index) => {
+                return index % 2 === 0 && el;
+              })
+              .map((el, index) => {
+                let objectKey = inputStringsHolder.filter((el, index) => {
+                  return index % 2 === 1 && el;
+                });
+                let objKey = objectKey[index];
+                return (
+                  <input
+                    key={index}
+                    style={{ marginTop: "10px" }}
+                    onChange={(e) => {
+                      let newObject = { ...hostelParametersObject };
+                      newObject[objKey] = e.target.value;
+                      setHostelObject(newObject);
+                    }}
+                    value={hostelParametersObject[objKey]}
+                    placeholder={el}
+                  />
+                );
+              })}
+            <button onClick={() => console.log(hostelParametersObject)}>
+              Добавить
+            </button>
           </div>
         )}
       </div>
