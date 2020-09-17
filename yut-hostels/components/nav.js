4fa696105e4c22dc62e89e950a0852157778c1f9
spanki22052 <useful22052@gmail.com>
2020-09-17 18:11:39 +0300

    repaired navbar problem

--- yut-hostels/components/nav.js
+++ yut-hostels/components/nav.js
@@ -4,49 +4,57 @@ import { useState } from "react";
 import HamburgerMenu from "react-hamburger-menu";
 
 export default function Nav({ children }) {
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
-	return (
-		<div className={classes.wrapper}>
-			<div className={classes.container}>
-				<div className={classes.nav}>
-					<Link href="/">
-						<a>
-							<div className={classes.navLogo}>Уют</div>
-						</a>
-					</Link>
-					<div className={classes.navBurger} style={{ zIndex: "3" }}>
-						<HamburgerMenu
-							isOpen={isMenuOpen}
-							menuClicked={() => {
-								setIsMenuOpen(!isMenuOpen);
-								document.querySelector("body").classList.toggle('lock');
-							}}
-							width={25}
-							height={20}
-							strokeWidth={2}
-							rotate={0}
-							color="black"
-							borderRadius={0}
-							animationDuration={0.5}
-						/>
-					</div>
-					<div
-						className={
-							!isMenuOpen
-								? classes.navMenu + " " + classes.active
-								: classes.navMenu
-						}
-					>
-						<Link className={"hamb"} href="/hostelslist">
-							<a>список хостелов</a>
-						</Link>
-						<Link href="/">
-							<a>о нас</a>
-						</Link>
-					</div>
-				</div>
-				<main>{children}</main>
-			</div>
-		</div>
-	);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <div className={classes.nav}>
+          <Link href="/">
+            <a>
+              <div className={classes.navLogo}>
+                <span onClick={() => setIsMenuOpen(false)}>Уют</span>
+              </div>
+            </a>
+          </Link>
+          <div className={classes.navBurger} style={{ zIndex: "3" }}>
+            <HamburgerMenu
+              isOpen={isMenuOpen}
+              menuClicked={() => {
+                setIsMenuOpen(!isMenuOpen);
+                document.querySelector("body").classList.toggle("lock");
+              }}
+              width={25}
+              height={20}
+              strokeWidth={2}
+              rotate={0}
+              color="black"
+              borderRadius={0}
+              animationDuration={0.5}
+            />
+          </div>
+          <div
+            className={
+              !isMenuOpen
+                ? classes.navMenu + " " + classes.active
+                : classes.navMenu
+            }
+          >
+            <Link className={"hamb"} href="/hostelslist">
+              <a>
+                <span onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                  список хостелов
+                </span>
+              </a>
+            </Link>
+            <Link href="/">
+              <a>
+                <span onClick={() => setIsMenuOpen(!isMenuOpen)}>о нас</span>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <main>{children}</main>
+      </div>
+    </div>
+  );
 }
