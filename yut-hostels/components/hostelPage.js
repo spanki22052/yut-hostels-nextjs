ccdf5ea7f2ae72484e410eb66fc9b0338f455b2b
spanki22052 <useful22052@gmail.com>
2020-09-17 23:56:41 +0300

    url hostelId

--- yut-hostels/components/hostelPage.js
+++ yut-hostels/components/hostelPage.js
@@ -1,72 +1,96 @@
 import classes from "../styles/hostelid.module.scss";
-import Nav from '../components/nav';
+import Nav from "../components/nav";
 import Slider from "react-slick";
 
+export default function HostelPage({ infoObject }) {
+  console.log(infoObject);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    // variableWidth: true
+  };
 
-
-export default function HostelPage() {
-
-	const settings = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		// variableWidth: true
-	};
-
-	return (
-		<div className={classes.hostelPage}>
-			<div className={classes.hostelMainInfo}>
-				<div className={classes.bookingInfo}>
-					<div className={classes.reservation}><img src="/svg/reservation.png"></img></div>
-					<div className={classes.booking}>Booking<span>.com</span></div>
-				</div>
-				<div className={classes.hostelInfo}>
-					<div className={classes.title}>Хостел “Диско”</div>
-					<div className={classes.suptitle}>Отличный, недорогой хостел в неплохом районе! Желающие, встречайте! Хостел лучший из лучших!</div>
-					<div className={classes.freePlaces}>Количество свободных мест</div>
-					<div className={classes.progressBar}>
-						<div
-							className={classes.progress}></div>
-					</div>
-					<div className={classes.procent}>75%</div>
-				</div>
-			</div>
-			<div className={classes.hostelSlider}>
-				<Slider {...settings}>
-					<div className={classes.sliderItem}>
-						<img src="/svg/hostels.png"></img>
-					</div>
-					<div className={classes.sliderItem}>
-						<img src="/svg/hostels.png"></img>
-					</div>
-					<div className={classes.sliderItem}>
-						<img src="/svg/hostels.png"></img>
-					</div>
-					<div className={classes.sliderItem}>
-						<img src="/svg/hostels.png"></img>
-					</div>
-				</Slider>
-			</div>
-			<div className={classes.location}>
-				<div className={classes.metro}>
-					<img className={classes.metroIcon} src="/svg/metro.svg" />
-					<div className={classes.hostelLocation}>м. Первомайское</div>
-					<div className={classes.stroka}></div>
-					<div className={classes.metroLocation}>350м ходьбы</div>
-					<div className={classes.metroLocation}>около 5 мин</div>
-				</div>
-				<div className={classes.phone}>
-					<div>Телефон</div>
-					<div className={classes.phoneInfo}>
-						<img src="/svg/phone.svg" />
-						<div>8-977-597-48-12</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-
+  return (
+    <>
+      {infoObject !== undefined && (
+        <div className={classes.hostelPage}>
+          <div className={classes.hostelMainInfo}>
+            <div className={classes.bookingInfo}>
+              <div className={classes.reservation}>
+                <img src="/svg/reservation.png"></img>
+              </div>
+              <div className={classes.booking}>
+                Booking<span>.com</span>
+              </div>
+            </div>
+            <div className={classes.hostelInfo}>
+              <div className={classes.title}>Хостел “{infoObject.title}”</div>
+              <div className={classes.suptitle}>{infoObject.description}</div>
+              <div className={classes.freePlaces}>
+                Количество свободных мест
+              </div>
+              <div className={classes.progressBar}>
+                <div className={classes.progress}></div>
+              </div>
+              <div className={classes.procent}>
+                {infoObject.peoplePercentage}%
+              </div>
+            </div>
+          </div>
+          <div className={classes.hostelSlider}>
+            <Slider {...settings}>
+              {infoObject.photos.length > 0 &&
+                infoObject.photos.map((el, index) => {
+                  return (
+                    <div key={index} className={classes.sliderItem}>
+                      <img src={"/svg/hostels.png"}></img>
+                    </div>
+                  );
+                })}
+            </Slider>
+          </div>
+          <div className={classes.location}>
+            <div className={classes.metro}>
+              <img className={classes.metroIcon} src="/svg/metro.svg" />
+              <div className={classes.hostelLocation}>
+                м. {infoObject.metro}
+              </div>
+              <div
+                className={classes.stroka}
+                style={{
+                  backgroundColor: `#${infoObject.hex}`,
+                  boxShadow: `0px 0px 6px 1px #${infoObject.hex}`,
+                }}
+              ></div>
+              <div className={classes.metroLocation}>350м ходьбы</div>
+              <div className={classes.metroLocation}>около 5 мин</div>
+            </div>
+            <div className={classes.phone}>
+              <div>Телефон</div>
+              <div className={classes.phoneInfo}>
+                <a
+                  href={`tel:${infoObject.phoneNumber}`}
+                  style={{ textDecoration: "none", color: "#988A7A" }}
+                >
+                  <img src="/svg/phone.svg" />
+                </a>
+                <div>
+                  <a
+                    href={`tel:${infoObject.phoneNumber}`}
+                    style={{ textDecoration: "none", color: "#988A7A" }}
+                  >
+                    {infoObject.phoneNumber}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
--- yut-hostels/pages/hostels/[hostelId].js
+++ yut-hostels/pages/hostels/[hostelId].js
@@ -2,12 +2,32 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import firebase from "../../components/firebaseDB";
 import Nav from "../../components/nav";
-import HostelPage from '../../components/hostelPage';
+import HostelPage from "../../components/hostelPage";
 
 export default () => {
-	return (
-		<Nav>
-			<HostelPage />
-		</Nav>
-	);
+  const router = useRouter().query.hostelId;
+  const [infoObject, setObject] = useState({});
+  const [infoBadges, setBadges] = useState({});
+  
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("hostels")
+      .doc("hostelsObject")
+      .get()
+      .then((info) => setObject(info.data().hostels));
+
+    firebase
+      .firestore()
+      .collection("hostels")
+      .doc("hostelBadges")
+      .get()
+      .then((info) => setBadges(info.data().hostels));
+  }, []);
+
+  return (
+    <Nav>
+      <HostelPage infoObject={infoObject[infoBadges[router]]} />
+    </Nav>
+  );
 };
