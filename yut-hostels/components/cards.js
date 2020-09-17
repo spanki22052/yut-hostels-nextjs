2760abe30ec5ebca1e0c597844961a2cbddcb3b6
spanki22052 <useful22052@gmail.com>
2020-09-17 11:26:53 +0300

    load info to mainpage slider

--- yut-hostels/components/cards.js
+++ yut-hostels/components/cards.js
@@ -1,17 +1,20 @@
 import classes from "../styles/cards.module.scss";
 
-export default function Card() {
-	return (
-		<>
-			<div className={classes.cardBlock}>
-				<div className={classes.cardTitle}>Диско</div>
-				<div className={classes.cardSupTitle}>Количество свободных мест</div>
-				<div className={classes.cardProgressBar}>
-					<div className={classes.Progress}></div>
-				</div>
-				<div>75%</div>
-				<img className={classes.icon} src="/svg/play.svg" />
-			</div>
-		</>
-	);
+export default function Card({ infoObject }) {
+  console.log(infoObject);
+  return (
+    <>
+      {Object.keys(infoObject).length > 0 && (
+        <div className={classes.cardBlock}>
+          <div className={classes.cardTitle}>{infoObject.title}</div>
+          <div className={classes.cardSupTitle}>Количество свободных мест</div>
+          <div className={classes.cardProgressBar}>
+            <div className={classes.Progress}></div>
+          </div>
+          <div>{infoObject.peoplePercentage}%</div>
+          <img className={classes.icon} src="/svg/play.svg" />
+        </div>
+      )}
+    </>
+  );
 }
--- yut-hostels/components/cardsSlider.js
+++ yut-hostels/components/cardsSlider.js
@@ -1,44 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import Card from "./cards";
-import classes from "../styles/cardsSlider.module.scss"
+import classes from "../styles/cardsSlider.module.scss";
+import firebase from "./firebaseDB";
 
-export default class SimpleSlider extends React.Component {
-	render() {
-		var settings = {
-			dots: false,
-			infinite: true,
-			speed: 500,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			variableWidth: true,
-			centerMode: true,
-			swipeToSlide: true,
-			touchThreshold: 10,
-		};
-		return (
-			<div className={classes.cardsSlider}>
-				<Slider {...settings}>
-					<div>
-						<Card />
-					</div>
-					<div>
-						<Card />
-					</div>
-					<div>
-						<Card />
-					</div>
-					<div>
-						<Card />
-					</div>
-					<div>
-						<Card />
-					</div>
-					<div>
-						<Card />
-					</div>
-				</Slider>
-			</div>
-		);
-	}
+export default function SimpleSlider() {
+  const [infoObject, setObject] = useState({});
+  const [infoList, setList] = useState({});
+
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    centerMode: true,
+    swipeToSlide: true,
+    touchThreshold: 10,
+  };
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("hostels")
+      .doc("hostelsObject")
+      .get()
+      .then((info) => setObject(info.data().hostels));
+
+    firebase
+      .firestore()
+      .collection("hostels")
+      .doc("hostelsList")
+      .get()
+      .then((info) => setList(info.data().hostels));
+  }, []);
+
+  return (
+    <div className={classes.cardsSlider}>
+      <Slider {...settings}>
+        {infoList.length > 0 &&
+          infoList.map((element, index) => {
+            return (
+              <div>
+                <Card key={index} infoObject={infoObject[element]} />
+              </div>
+            );
+          })}
+      </Slider>
+    </div>
+  );
 }
