823de13566c5909459bc870ba09a4ce63bc6cd69
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-09-18 17:43:20 +0300

    for boss

--- yut-hostels/pages/_app.js
+++ yut-hostels/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/reset.scss';
 import "slick-carousel/slick/slick.css";
-
 import "slick-carousel/slick/slick-theme.css";
+import 'react-medium-image-zoom/dist/styles.css'
 
 
 export default function MyApp({ Component, pageProps }) {
