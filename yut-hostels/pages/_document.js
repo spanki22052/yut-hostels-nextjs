889313a15fa8ef21ab0ed48ff40d5724b3411a35
spanki22052 <useful22052@gmail.com>
2020-09-18 21:13:55 +0300

    add some meta

--- yut-hostels/pages/_document.js
+++ yut-hostels/pages/_document.js
@@ -1,28 +1,50 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx)
-		return { ...initialProps }
-	}
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-	render() {
-		return (
-			<Html>
-				<Head>
-					<meta name="google-site-verification" content="TRP0wsyEX7Ax48uNjE8ErWw0AU6oW_gFFkyqmnfIVAc" />
-					<link href="dist/hamburgers.css" rel="stylesheet" />
-					<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;700&display=swap" rel="stylesheet"></link>
-					<link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-					<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-					<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="TRP0wsyEX7Ax48uNjE8ErWw0AU6oW_gFFkyqmnfIVAc"
+          />
+          <meta
+            name="description"
+            content="Комфортное и доступное жилье в центре Москвы."
+          />
+
+          <link href="dist/hamburgers.css" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
