2d62f8f60e3c58675d40208950c56ebdc77fe64c
spanki22052 <useful22052@gmail.com>
2020-09-29 15:14:35 +0300

    sitemap

--- yut-hostels/pages/_document.js
+++ yut-hostels/pages/_document.js
@@ -11,23 +11,43 @@ export default class MyDocument extends Document {
 			<Html>
 				<Head>
 					<script type="text/javascript" jsx>{`
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+						(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+						m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+						(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(67478275, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });`}
+						ym(67478275, "init", {
+								clickmap:true,
+								trackLinks:true,
+								accurateTrackBounce:true
+					});`}
 					</script>
+					<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+ <url>
+  <loc>http://mysite.ru/</loc>
+  <lastmod>2014-09-18T18:54:13+04:00</lastmod>
+  <changefreq>always</changefreq>
+  <priority>1.0</priority>
+ </url>
+ <url>
+  <loc>http://mysite.ru/category/</loc>
+  <lastmod>2014-09-18T18:57:09+04:00</lastmod>
+  <changefreq>hourly</changefreq>
+  <priority>0.8</priority>
+ </url>
+ <url>
+  <loc>http://mysite.ru/page/</loc>
+  <lastmod>2014-09-18T18:59:37+04:00</lastmod>
+  <changefreq>daily</changefreq>
+  <priority>0.6</priority>
+ </url>
+</urlset>
 					{/* <noscript><div><img src="https://mc.yandex.ru/watch/67478275" style="position:absolute; left:-9999px;" alt="" /></div></noscript> */}
 					<meta name="language" content="ru" />
 					{/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
 					<meta name="googlebot-news" content="nosnippet" />
 					<meta name="yandex-verification" content="fd16859da2abddbd" />
 					<meta
-						name="google-site-verification"
+						name="google-site-verification"		
 						content="FTe50ALqpfNJHROZTMKhdNctXVlC0K7y0XmTkfZh-SE"
 					/>
 					<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
